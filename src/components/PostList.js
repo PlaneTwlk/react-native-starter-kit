c8204808a8e6005ced7acd3e57b4e6e63d62e4d7
Plane <planetw.git@gmail.com>
2016-07-26 19:58:50 +0800

    Fix post flex box

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react';
-import { refreshControl } from 'react-native';
-import { List, ListItem, Text } from 'native-base';
+import { StyleSheet } from 'react-native';
+import { List, ListItem, View, Text } from 'native-base';
 import _ from 'lodash';
 
 const PostList = (props) => {
@@ -8,10 +8,19 @@ const PostList = (props) => {
   return (
     <List>
       {
-        _.map(rows, item=> {
+        _.map(rows, (item, key)=> {
           return(
-            <ListItem key={item.id}>
-              <Text>{item.title}</Text>
+            <ListItem key={ key } style={ styles.listItem }>
+              <View>
+                <View style={ styles.authorBar }>
+                  <Text style={ styles.author }>{ item.name }</Text>
+                  <Text style={ styles.dateTime }>{ item.dateTime }</Text>
+                </View>
+                <View style={ styles.messageWrap}>
+                  <Text style={ styles.title }>{ item.title }</Text>
+                  <Text style={ styles.content }>{ item.content }</Text>
+                </View>
+              </View>
             </ListItem>
           );
         })
@@ -25,3 +34,40 @@ PostList.props = {
 };
 
 export default PostList;
+
+const styles = StyleSheet.create({
+  listItem: {
+    flexDirection: null,
+    marginLeft: null,
+    padding: null,
+    paddingLeft: null
+  },
+  authorBar: {
+    backgroundColor: '#E0E0E0',
+    paddingBottom: 5,
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingTop: 5
+  },
+  author:{
+    lineHeight: null,
+  },
+  dateTime: {
+    color: 'gray',
+    fontSize: 12,
+    lineHeight: null,
+  },
+  messageWrap: {
+    paddingLeft: 20,
+    paddingRight: 20
+  },
+  title: {
+    marginTop: 5,
+    marginBottom: 5
+  },
+  content: {
+    color: '#5B5B5B',
+    fontSize: 13,
+    marginBottom: 5
+  }
+});
